ca1c4de46c5774b6d3a2f1b1b60e80622a70d2c1
WanilkaGIT <mahlunivan27@gmail.com>
2024-09-21 22:19:45 +0300

    lesson is over

--- booking/urls.py
+++ booking/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from booking import views
 
 urlpatterns = [
-    path("rooms/", views.room_list, name="room-list")
+    path("rooms/", views.room_list, name="room-list"),
+    path("book-room/", views.booking_room, name="book-room"),
+    path("booking-details/<int:pk>/", views.booking_details, name="booking-details"),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Room
+from django.shortcuts import render, redirect
+from .models import Room, Booking
+from django.http import HttpResponse
 
 # Create your views here.
 def room_list(request):
@@ -7,3 +8,33 @@ def room_list(request):
     context = {
         "rooms": rooms,}
     return render(request, "booking/room_list.html", context)
+
+def booking_room(request):
+    if request.method == "POST":
+        start_time = request.POST.get("start-time")
+        end_time = request.POST.get("end-time")
+        room_number = request.POST.get("room-number")
+
+        try:
+            room = Room.objects.get(number=room_number)
+        except Room.DoesNotExist:
+            HttpResponse("Room doesn't exist", status_code=404)
+
+        booking = Booking.objects.create(
+            user = request.user,
+            room = room,
+            start_time = start_time,
+            end_time = end_time)
+        
+        return redirect("booking-details", pk=booking.id)
+    
+    else:
+        return render(request, "booking/booking_form.html")
+
+def booking_details(request, pk):
+    booking = Booking.objects.get(id=pk)
+
+    context = {
+        "booking": booking}
+    
+    return render(request, "booking/booking_details.html", context)
